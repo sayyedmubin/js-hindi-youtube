6833063757d91d16b4a43606e56e8c8063a7da47
sayyedmubin <116707539+sayyedmubin@users.noreply.github.com>
2024-02-07 15:44:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) we are using node.js, not browser
+
+console.log(3 
+    + 
+    3)  // code readability should be high
+
+console.log("Mubin")
+
+
+let name = "mubin"
+let age  = 18
+let isloggedIn = false
+
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ //symbol => unique
+
+
+ //object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //object
+
+
+
+
