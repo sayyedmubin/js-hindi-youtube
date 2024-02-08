1387b3e4820d5dbd7bfe954eb102b568b06b7b21
sayyedmubin <116707539+sayyedmubin@users.noreply.github.com>
2024-02-08 12:41:17 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isloggedIn = "0"
 
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1  => true; 0 => false
 // "" => false
@@ -24,5 +24,45 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ********************** Operations ******************************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 =  "mubin"
+let str3 =str1 +  str2
+
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log( (3+4) *5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
