31c15bd08da2c9ad898cda19f2b3a2b6db601892
sayyedmubin <116707539+sayyedmubin@users.noreply.github.com>
2024-02-10 18:54:07 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//(1) primitive : all primitive datatypes are call by value(whenever we copy any data of primitive we cant get original data of refence will not give of memory it will give copy)
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const another = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//const bigNumber = 1216465323262323232n
+
+// Reference (Non primitive)
+
+// Array, objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Mubin",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+//https://262.ecma-international.org.5.1/#sec-11.4.3
